7ef4a1999bb65913341802c3239e24c790f01963
Lam <dlphan.qn@outlook.com>
2020-07-07 17:30:16 +0700

    add polyfill

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
+import 'core-js'
+import 'raf/polyfill'
+import '@babel/polyfill'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PureApp from './App'
